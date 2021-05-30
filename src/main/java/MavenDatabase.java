f0027d5a7f89baee6978c6853ecd6deaa7f913e1
motsu <s1129205@student.hsleiden.nl>
2021-05-30 20:26:15 +0200

    i was wondering if after all these years you would like to meet..

--- src/main/java/MavenDatabase.java
+++ src/main/java/MavenDatabase.java
@@ -1,6 +1,29 @@
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import java.io.IOException;
+
 public class MavenDatabase {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.getApplicationDefault())
+                .setDatabaseUrl("https://firebase-adminsdk-w2fgi@risk14.iam.gserviceaccount.com/")
+                //.setProjectId(JVdjohjEM9i80EO4iATR)
+                .build();
+
+//        FileInputStream refreshToken = new FileInputStream("risk14-firebase-adminsdk-w2fgi-89c33e4d8e.json");
+        FirebaseApp.initializeApp(options);
+
+//        // Initialize the default app
+//        FirebaseApp defaultApp = FirebaseApp.initializeApp(defaultOptions);
+//
+//        System.out.println(defaultApp.getName());  // "[DEFAULT]"
+//
+//        // Retrieve services by passing the defaultApp variable...
+//        FirebaseAuth defaultAuth = FirebaseAuth.getInstance(defaultApp);
+//        FirebaseDatabase defaultDatabase = FirebaseDatabase.getInstance(defaultApp);
 
     }
 }
