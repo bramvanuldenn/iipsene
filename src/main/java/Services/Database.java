3cfc7b01c0a8fb2fdb82ecb59743df6522c68b44
Hayyan55 <hayan_mez@hotmail.co.uk>
2021-06-13 14:47:38 +0200

    i don't understand any fucking thing in IT

--- src/main/java/Services/Database.java
+++ src/main/java/Services/Database.java
@@ -6,71 +6,44 @@ import com.google.cloud.firestore.*;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
+import org.apache.log4j.PropertyConfigurator;
 
-import javax.annotation.Nullable;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 
 
 public class Database {
     private static Firestore db;
 
-    public static void firestore() throws IOException, ExecutionException, InterruptedException {
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+        initPlayer();
+    }
+
+    public static void initPlayer() throws IOException, ExecutionException, InterruptedException {
         FileInputStream refreshToken = new FileInputStream(System.getProperty("user.dir") + "/risk14-firebase-adminsdk-w2fgi-89c33e4d8e.json");
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(refreshToken))
                 .build();
 
-        FirebaseApp.initializeApp(options);
-
-        Database.db = FirestoreClient.getFirestore();
-
-//        //asynchronously retrieve all documents
-//        ApiFuture<QuerySnapshot> future = db
-//                .collection("players").get();
-//
-//        // future.get() blocks on response
-//        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
-//        for (QueryDocumentSnapshot document : documents) {
-//            System.out.println(document.getData());
-//        }
-        db.collection("cities")
-                .whereEqualTo("state", "CA")
-                .addSnapshotListener(new EventListener<QuerySnapshot>() {
-                    @Override
-                    public void onEvent(@Nullable QuerySnapshot snapshots, @Nullable FirestoreException e) {
-                        if (e != null) {
-                            System.err.println("Listen failed:" + e);
-                            return;
-                        }
-
-                        List<String> cities = new ArrayList<>();
-                        for (DocumentSnapshot doc : snapshots) {
-                            if (doc.get("name") != null) {
-                                cities.add(doc.getString("name"));
-                            }
-                        }
-                        System.out.println("Current cites in CA: " + cities);
-                    }
-                });
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("give a new name without spaces");
+        String name = input.next();
+        System.out.println("give a new id");
+        int id = input.nextInt();
         // Object dat je gaat schrijven naar de database
-        Map<String, String> map = new HashMap<>();
-        map.put("name", "Koen");
-        map.put("age", "20");
-        map.put("city", "Woerden");
+        Map<String, Integer> map = new HashMap<>();
+        map.put(name,id);
 
-        System.out.println(map);
-
-        // Data schrijven
-        db.collection("players").document("Hayyan").set(map);
-
-        // Specifieke data lezen
-        System.out.println(db.collection("players").document("info").get().get().getData());
+        FirebaseApp.initializeApp(options);
+        db = FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> docRef = db
+                .collection("players")
+                .document("info")
+                .update(name, id);
+        System.out.println("Update time : " + docRef.get().getUpdateTime());
     }
+
+    public void
 }
