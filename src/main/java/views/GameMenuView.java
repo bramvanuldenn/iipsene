0d8a1c573f1bae2a0183a9db4528fbd716be1573
Encoded <pepijndeoude@gmail.com>
2021-06-15 12:20:19 +0200

    Fixed menu size

--- src/main/java/views/GameMenuView.java
+++ src/main/java/views/GameMenuView.java
@@ -6,10 +6,16 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 
 import controllers.BoardController;
@@ -22,10 +28,14 @@ public class GameMenuView {
     private Scene scene;
     private Parent root;
 
+
     @FXML
-    protected void initialize(){
+    protected void initialize() throws FileNotFoundException {
         this.boardController = BoardController.getInstance();
     }
 
 
+    public void settingsButtonPressed(MouseEvent event) {
+        System.out.println("button pressed");
+    }
 }
