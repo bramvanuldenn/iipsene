8f9e1b5ea6ecf6cd7d1ec96d096a1cf78360d856
Encoded <pepijndeoude@gmail.com>
2021-06-16 14:14:57 +0200

    gameview added back

--- src/main/java/views/MainMenuView.java
+++ src/main/java/views/MainMenuView.java
@@ -1,22 +1,32 @@
 package views;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import controllers.MainMenuController;
+import javafx.stage.Window;
 import services.FirebaseService;
 import shared.MenuObservable;
 import shared.MenuObserver;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class MainMenuView implements MenuObserver {
     MainMenuController mainMenuController;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
     @FXML
     TextField enteredName = new TextField();
@@ -28,13 +38,16 @@ public class MainMenuView implements MenuObserver {
     }
 
     public void setGameMenu() {
-        /*
-        this.stage = (Stage)((Node) event.getSource()).getScene().getWindow();
-        this.scene = new Scene(root, 1000, 800);
-        stage.setScene(scene);
-        stage.show();
-         */
-        System.out.println("set game menu");
+        try{
+            this.root = FXMLLoader.load(getClass().getClassLoader().getResource("gamemenu.fxml"));
+            this.stage = (Stage) enteredName.getScene().getWindow();
+            this.scene = new Scene(root, 1000, 800);
+            this.stage.setScene(this.scene);
+            this.stage.show();
+            System.out.println("set game menu");
+        } catch (IOException ex) {
+            System.err.println("Error reading file");
+        }
     }
 
     public void startGamePressed(MouseEvent event) throws IOException, ExecutionException, InterruptedException {
